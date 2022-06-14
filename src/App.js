8c1bccfada2b6ca56b6356bfcb927f70eed49103
deniso4ka <den.zagrosh@gmail.com>
2022-06-14 21:16:14 +0300

    jsx and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+
+
+import Header from './components/Header/Header'
+import Categories from './components/Categories/Categories'
+import Sort from './components/Sort/Sort'
 
 function App() {
   return (
-    <div className="App">
-      1
+    <div class='wrapper'>
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,21 @@
+import './Categories.scss'
+
+
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+
+  )
+}
+
+export default Categories
